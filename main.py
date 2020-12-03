4f995247854e5cb2ed859fc02a7228bc9da97236
mayank160920 <62462874+mayank160920@users.noreply.github.com>
2020-12-03 12:41:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/') # this is the home page route
+def hello_world(): # this is the home page function that generates the page code
+    return "Hello world!"
+   
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080) # This line is required to run Flask on repl.it
