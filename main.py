dfa43ff431fd6f3f75ba7e65732e4ec3c7c15660
mayank160920 <62462874+mayank160920@users.noreply.github.com>
2021-05-27 22:48:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,33 @@ def hello_world(): # this is the home page function that generates the page code
 
 @app.route('/ep1') 
 def ep1(): # this is the home page function that generates the page code
-    time.sleep(4)
+    time.sleep(5)
     return "ep1"
 
+@app.route('/ep2') 
+def ep2(): # this is the home page function that generates the page code
+    time.sleep(10)
+    return "ep1"
+
+@app.route('/ep3') 
+def ep3(): # this is the home page function that generates the page code
+    time.sleep(15)
+    return "ep1"
+
+@app.route('/ep4') 
+def ep4(): # this is the home page function that generates the page code
+    time.sleep(20)
+    return "ep1"
+
+@app.route('/ep5') 
+def ep5(): # this is the home page function that generates the page code
+    time.sleep(25)
+    return "ep1"
+
+@app.route('/ep6') 
+def ep6(): # this is the home page function that generates the page code
+    time.sleep(30)
+    return "ep1"
    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080) # This line is required to run Flask on repl.it
